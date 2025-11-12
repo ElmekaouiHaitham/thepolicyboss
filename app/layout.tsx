1f10b158d558be5df4fea8eebb140fbcfc1a8183
haitham <haithamelmekaoui@gmail.com>
2025-11-12 22:56:16 +0100

    Add @vercel/analytics dependency and integrate analytics component in layout; remove unused hero images

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -91,6 +92,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
